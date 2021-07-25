a0bd55e2849134be31711e6323fabb061e0769bd
Jonathan Ingram <scottingram.scott@gmail.com>
2021-07-25 19:32:43 -0400

    Worked out logic flow of js script.
    Worked out the basic app using console.log() and added some validation.

--- js/site.js
+++ js/site.js
@@ -1,4 +1,32 @@
 // Call Hellow World
-function HelloWorld() {
-    alert("Hellow World!");
+let startingNumber, endingNumber;
+
+function displayValues() {
+    startingNumber = document.getElementById("startValue").value;
+    endingNumber = document.getElementById("endValue").value;
+    
+    if(startingNumber != "" && endingNumber != "") {
+        console.log("Starting Value: " + startingNumber);
+        console.log("Ending Value: " + endingNumber);
+
+        if (startingNumber > endingNumber) {
+            alert("Error: Starting number must be less than ending number!");
+        } else {
+            for (let i = startingNumber; i <= endingNumber; i++) {
+                if(i % 2 == 0) {
+                    if(i == 0){
+                        console.log(i);
+                    }else {
+                        console.log(i + " is even!");
+                    }
+                    
+                } else {
+                    console.log(i);
+                }
+              }
+        }
+
+    } else {
+        alert("Error: Make sure both values are not empty!")
+    }
 }
