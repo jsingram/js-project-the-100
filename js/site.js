5e5ccaad5b2545a6d6831e358ee930087fc52a36
Jonathan Ingram <scottingram.scott@gmail.com>
2021-07-25 20:25:28 -0400

    My Version
    Has a weird input bug I haven't figured out yet.

--- js/site.js
+++ js/site.js
@@ -1,30 +1,30 @@
-// Call Hellow World
-let startingNumber, endingNumber;
+
 
 function displayValues() {
-    startingNumber = document.getElementById("startValue").value;
-    endingNumber = document.getElementById("endValue").value;
-    
-    if(startingNumber != "" && endingNumber != "") {
-        console.log("Starting Value: " + startingNumber);
-        console.log("Ending Value: " + endingNumber);
+    let startingNumber, endingNumber;
+let resultsTable = document.getElementById("results");
 
-        if (startingNumber > endingNumber) {
-            alert("Error: Starting number must be less than ending number!");
-        } else {
+    startingNumber = document.getElementById("startValue").value;
+    endingNumber = document.getElementById("endValue").value; 
+
+    if(startingNumber != "" && endingNumber != "") {
+        
+
+       
+            resultsTable.innerHTML = "";
             for (let i = startingNumber; i <= endingNumber; i++) {
                 if(i % 2 == 0) {
-                    if(i == 0){
-                        console.log(i);
-                    }else {
+                    
                         console.log(i + " is even!");
-                    }
+                        resultsTable.innerHTML = resultsTable.innerHTML + "<tr><td><strong>" + i + "</strong></td></tr>"
+                    
                     
                 } else {
                     console.log(i);
+                    resultsTable.innerHTML = resultsTable.innerHTML + "<tr><td>" + i + "</td></tr>"
                 }
               }
-        }
+        
 
     } else {
         alert("Error: Make sure both values are not empty!")
