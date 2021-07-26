2ff73dd2939a44f97a3f42d18f11ab4f00452f77
Jonathan Ingram <scottingram.scott@gmail.com>
2021-07-25 23:07:05 -0400

    Added error handling. Finished app.

--- js/site.js
+++ js/site.js
@@ -1,32 +1,67 @@
+// Retrieve values from form.
+function getValues() {
+    // Get values from form.
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+    let errorStartGreaterThanEnd = '<tr class="table-danger"><td>Starting interger must be smaller than ending integer.</td></tr>';
+    let errorFieldBlank = '<tr class="table-danger"><td>Please enter a number in both fields.</td></tr>';
+    let errorNumOutOfBounds = '<tr class="table-danger"><td>Numbers are out of bounds. Please enter numbers from 0 to 100.</td></tr>';
+    
+    // Convert input to integers
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
 
+    // Validate input and make sure they are integers
+    if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+        if(startValue > endValue) {
+            document.getElementById("resultsTable").innerHTML = errorStartGreaterThanEnd;
+        } else if (startValue < 0 || endValue > 100) {
+            document.getElementById("resultsTable").innerHTML = errorNumOutOfBounds;
+        } else {
+            // Call generateNumbers()
+            let numbers = generateNumbers(startValue, endValue);
 
-function displayValues() {
-    let startingNumber, endingNumber;
-let resultsTable = document.getElementById("results");
-
-    startingNumber = document.getElementById("startValue").value;
-    endingNumber = document.getElementById("endValue").value; 
-
-    if(startingNumber != "" && endingNumber != "") {
+            // Call displayResults()
+            displayResults(numbers);
+        }
         
-
-       
-            resultsTable.innerHTML = "";
-            for (let i = startingNumber; i <= endingNumber; i++) {
-                if(i % 2 == 0) {
-                    
-                        console.log(i + " is even!");
-                        resultsTable.innerHTML = resultsTable.innerHTML + "<tr><td><strong>" + i + "</strong></td></tr>"
-                    
-                    
-                } else {
-                    console.log(i);
-                    resultsTable.innerHTML = resultsTable.innerHTML + "<tr><td>" + i + "</td></tr>"
-                }
-              }
-        
-
     } else {
-        alert("Error: Make sure both values are not empty!")
+        document.getElementById("resultsTable").innerHTML = errorFieldBlank;
     }
+}
+
+// Generate range of numbers based off of entered values.
+function generateNumbers(startValue, endValue) {
+    let numbers = [];
+
+    // get numbers from start to end
+    for (let index = startValue; index <= endValue; index++) {
+        numbers.push(index);
+    }
+
+    return numbers;
+}
+
+// Display numbers and make even ones bold.
+function displayResults(numbers) {
+    let templateRows = "";
+
+    for (let index = 0; index < numbers.length; index++) {
+        let number = numbers[index];
+        if(number%2 == 0) {
+            /* ===============================================================
+               Blender does not display template literals very well. See source
+               code for referece to this line of code. 
+               =============================================================== */
+            templateRows += `<tr><td><strong>${number}</strong></td></tr>`;
+        }else {
+            /* ===============================================================
+               Blender does not display template literals very well. See source
+               code for referece to this line of code. 
+               =============================================================== */
+            templateRows += `<tr><td>${number}</td></tr>`;
+        }
+    }
+
+    document.getElementById("resultsTable").innerHTML=templateRows;
 }
